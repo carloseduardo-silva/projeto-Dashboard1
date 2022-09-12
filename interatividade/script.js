0c9f1470a7a7c57cbd627954e758a2b597d75df6
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2022-09-12 14:16:02 -0300

    mobile first ok - starting css and media Q

---
+++ interatividade/script.js
@@ -0,0 +1,10 @@
+/* seletores */
+const lightbtn = document.querySelector('#lightmode')
+const darkbtn = document.querySelector('#darkmode')
+const btn = document.querySelector('#lightbtn')
+
+/* Dark/Light Button */
+function button() {
+lightbtn.classList.toggle('darkmode')
+
+}
